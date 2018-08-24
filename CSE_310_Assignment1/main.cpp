5550a157cdc0cdd551a4aa690b08ad0ee1f79089
dbasina <dbasina@asu.edu>
2018-08-23 17:59:08 -0700

    Initial Commit

---
+++ CSE_310_Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE_310_Assignment1
+//
+//  Created by Divesh Basina on 8/23/18.
+//  Copyright © 2018 ASU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
