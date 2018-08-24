e687c7f4f342f26e1a53a2564950008e335f856a
dbasina <dbasina@asu.edu>
2018-08-23 18:06:21 -0700

    Test 2

--- CSE_310_Assignment1/Assignment1.cpp
+++ CSE_310_Assignment1/Assignment1.cpp
@@ -5,19 +5,3 @@
 //  Created by Divesh Basina on 8/23/18.
 //  Copyright © 2018 ASU. All rights reserved.
 //
-
-//
-//  main.cpp
-//  CSE_310_Assignment1
-//
-//  Created by Divesh Basina on 8/23/18.
-//  Copyright © 2018 ASU. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
